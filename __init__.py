6ffcb0ab0baf664a90feb1ccadb11e49169b2901
jnphilipp <mail@jnphilipp.org>
2017-11-13 20:47:27 +0100

    Add LICENSE.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016-1017 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of django_bootstrap3.
+#
+# django_bootstrap3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# django_bootstrap3 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with django_bootstrap3. If not, see <http://www.gnu.org/licenses/>.
 
-default_app_config = 'django-bootstrap3.apps.DjangoBootstrap3Config'
+default_app_config = 'django_bootstrap3.apps.DjangoBootstrap3Config'
