560fb6df8779e209acecb6b7d4a0cd4a93cde857
jnphilipp <mail@jnphilipp.org>
2017-12-01 15:21:13 +0100

    Add append_attr and add_class filters.

--- templatetags/bootstrap.py
+++ templatetags/bootstrap.py
@@ -16,8 +16,50 @@
 # You should have received a copy of the GNU General Public License
 # along with django_bootstrap3. If not, see <http://www.gnu.org/licenses/>.
 
+import types
+
+from copy import copy
 from django.template import Library
 register = Library()
+
+
+def _process_field_attributes(field, attr, process):
+    # split attribute name and value from 'attr:value' string
+    params = attr.split(':', 1)
+    attribute = params[0]
+    value = params[1] if len(params) == 2 else ''
+
+    field = copy(field)
+
+    # decorate field.as_widget method with updated attributes
+    old_as_widget = field.as_widget
+
+    def as_widget(self, widget=None, attrs=None, only_initial=False):
+        attrs = attrs or {}
+        process(widget or self.field.widget, attrs, attribute, value)
+        html = old_as_widget(widget, attrs, only_initial)
+        self.as_widget = old_as_widget
+        return html
+
+    field.as_widget = types.MethodType(as_widget, field)
+    return field
+
+
+@register.filter('append_attr')
+def append_attr(field, attr):
+    def process(widget, attrs, attribute, value):
+        if attrs.get(attribute):
+            attrs[attribute] += ' ' + value
+        elif widget.attrs.get(attribute):
+            attrs[attribute] = widget.attrs[attribute] + ' ' + value
+        else:
+            attrs[attribute] = value
+    return _process_field_attributes(field, attr, process)
+
+
+@register.filter(name='add_class')
+def add_class(field, css_class):
+    return append_attr(field, 'class:' + css_class)
 
 
 @register.inclusion_tag('bootstrap/css.html')
