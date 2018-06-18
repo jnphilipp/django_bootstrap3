6edf4fcf9aaf25aeaaaf251adab1bd9098dece59
maxbry <github@mbryan.de>
2018-06-18 16:42:22 +0200

    Added check if page has number property

--- templatetags/bootstrap.py
+++ templatetags/bootstrap.py
@@ -87,8 +87,11 @@ def messages(context):
 
 @register.inclusion_tag('bootstrap/pagination.html', takes_context=True)
 def pagination(context, paginator, page, title=None, *args, **kwargs):
-    start_page = max(int(page.number) - 4, 0)
-    end_page = min(int(page.number) + 3, paginator.num_pages)
+    if not isinstance(page, int):
+        page = page.number
+    
+    start_page = max(page - 4, 0)
+    end_page = min(page + 3, paginator.num_pages)
     context['prange'] = paginator.page_range[start_page:end_page]
     context['page'] = page
     context['title'] = title
